d030b23c2263a53bca8d56fd80cb5d8ffba028e3
chen_ts <13813641925@163.com>
2020-07-12 21:54:25 +0800

    将system子模块，配置成最简单的Springboot项目

---
+++ system/src/main/java/com/cw/system/SystemApplication.java
@@ -0,0 +1,22 @@
+package com.cw.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class SystemApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(SystemApplication.class);
+
+    public static void main(String[] args) {
+       // SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication app = new SpringApplication(SystemApplication.class);
+        ConfigurableEnvironment env =app.run(args).getEnvironment();
+        logger.info("启动成功！");
+        logger.info("System地址: \thttp://127.0.0.1:{}",env.getProperty("server.port"));
+    }
+
+}
