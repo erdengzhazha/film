4b8688dc6c2833e2e116103d92ced8568e5edf83
chen_ts <13813641925@163.com>
2020-07-14 23:47:22 +0800

    1. 新增server子模块

---
+++ server/src/main/java/com/cw/server/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.cw.server.mapper;
+
+import com.cw.server.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    public List<Test> list();
+}
---
+++ server/src/main/java/com/cw/server/service/TestService.java
@@ -0,0 +1,19 @@
+package com.cw.server.service;
+
+import com.cw.server.domain.Test;
+import com.cw.server.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list(){
+        return testMapper.list();
+    }
+}
